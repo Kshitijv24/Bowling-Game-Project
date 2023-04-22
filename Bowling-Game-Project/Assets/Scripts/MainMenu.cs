1050fc0fdf173ae34de62a7907bccf1982cdca4c
Kshitijv24 <kshitijv24@gmail.com>
2023-04-22 17:03:32 +0530

    Added Main Menu in the game
    in which I also added credits screen

---
+++ Bowling-Game-Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject creditPannel;
+    bool isOpen = false;
+
+    private void Start()
+    {
+        creditPannel.SetActive(false);
+    }
+
+    public void LoadMainGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void CreditPannel()
+    {
+        isOpen = !isOpen;
+        creditPannel.SetActive(isOpen);
+    }
+}
