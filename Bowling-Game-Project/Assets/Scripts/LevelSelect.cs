86d07b6c589d79af1ff9b363638a6748d52bb8bc
Kshitijv24 <kshitijv24@gmail.com>
2023-04-28 11:08:17 +0530

    Added an Select Level Screen

---
+++ Bowling-Game-Project/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+
+    public void LoadLevel5()
+    {
+        SceneManager.LoadScene("Level5");
+    }
+    public void LoadLevel6()
+    {
+        SceneManager.LoadScene("Level6");
+    }
+    public void LoadLevel7()
+    {
+        SceneManager.LoadScene("Level7");
+    }
+    public void LoadLevel8()
+    {
+        SceneManager.LoadScene("Level8");
+    }
+    public void LoadLevel9()
+    {
+        SceneManager.LoadScene("Level9");
+    }
+    public void LoadLevel10()
+    {
+        SceneManager.LoadScene("Level10");
+    }
+    public void LoadLevel11()
+    {
+        SceneManager.LoadScene("Level11");
+    }
+    public void LoadLevel12()
+    {
+        SceneManager.LoadScene("Level12");
+    }
+}
